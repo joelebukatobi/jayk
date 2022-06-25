74543e01cf4ed6c23be206553f000090629b51b4
joelebukatobi <joelebuka@gmail.com>
2022-06-25 03:50:25 +0100

    Added Typed.js to the hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,23 @@
+import React, { useRef, useEffect } from 'react';
 import Link from 'next/link';
 import Button from '@/elements/Button';
+import Typed from 'typed.js';
 
 export default function Hero({ id, className, innerRef }) {
+  const typeTarget = useRef(null);
+
+  useEffect(() => {
+    const typed = new Typed(typeTarget.current, {
+      strings: ['Dissertation^1000', 'Thesis^1000', 'Paper^1000'],
+      typeSpeed: 120,
+      showCursor: false,
+      backSpeed: 100,
+    });
+
+    return () => {
+      typed.destroy();
+    };
+  }, []);
   return (
     <section
       id={id}
@@ -13,7 +29,7 @@ export default function Hero({ id, className, innerRef }) {
       </div>
       <div className="flex flex-col space-y-[2.4rem] items-center w-[100% md:w-[50%]">
         <h1 className="font-black w-[100%] text-[6rem] md:text-[5rem] xl:text-[7rem] marker:leading-[8.4rem]">
-          Writing a <span className="text-orange">Dissertation</span> has never been easier
+          Writing a <span className="text-orange text" ref={typeTarget}></span> has never been easier
         </h1>
         <p className="w-[100%] md:w-[100%] text-[2.4rem] font-medium">Need help with your essay? 👇🏽</p>
         <Link href="/#contact" passHref>
