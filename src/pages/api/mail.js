55bcd2b74811cf0634f156f5cdf7391497eb7d70
joelebukatobi <joelebuka@gmail.com>
2022-07-07 16:05:39 +0100

    Added Reviews Pop up

--- src/pages/api/mail.js
+++ src/pages/api/mail.js
@@ -6,10 +6,31 @@ export default async (req, res) => {
   try {
     await mail.send({
       to: 'jay_academy@yahoo.com',
-      from: req.body.email,
+      from: 'jennyijeife@gmail.com',
       subject: req.body.subject,
       text: req.body.message,
-      html: req.body.message.replace(/\r\n/g, '<br>'),
+      html: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+      <html lang="en">
+      <head>
+        <meta charset="utf-8">
+      
+        <title>The HTML5 Herald</title>
+        <meta name="description" content="Jayk Academy">
+      <meta http-equiv="Content-Type" content="text/html charset=UTF-8" />     
+      </head>
+      
+      <body>
+        <div class="img-container" style="display: flex;justify-content: center;align-items: center;border-radius: 5px;overflow: hidden; font-family: 'helvetica', 'ui-sans';">              
+              </div>
+              <div class="container" style="margin-left: 20px;margin-right: 20px;">
+              <h3>You've got a new mail from ${req.body.name}, their email is: ✉️${req.body.email} </h3>
+              <div style="font-size: 16px;">
+              <p>Message:</p>
+              <p>${req.body.message}</p>
+              </div>
+              </div>
+      </body>
+      </html>`,
     });
   } catch (error) {
     return res.status(error.statusCode || 500).json({ status: error.message });
