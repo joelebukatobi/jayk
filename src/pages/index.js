72f720c78592e68a01439eb12e81efc1520a1b3e
joelebukatobi <joelebuka@gmail.com>
2022-07-23 08:01:17 +0100

    Made changes to SEO

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import { useRef } from 'react';
+import Head from 'next/head';
 import Navbar from '@/components/Navbar';
 import Hero from '@/components/Hero';
 import Workflow from '@/components/Workflow';
@@ -22,6 +23,25 @@ export default function Home() {
   });
   return (
     <>
+      <Head>
+        <title>JayK Academy</title>
+        <meta
+          name="description"
+          content="Jayk Academy is a private research Consultancy firm that specializes in academic research writing and has other subsidiaries which help promote vocational and skill acquisition training."
+        />
+        <meta property="og:title" content="Jayk Academy" />
+        <meta
+          property="og:description"
+          content="Jayk Academy is a private research Consultancy firm that specializes in academic research writing and has other subsidiaries which help promote vocational and skill acquisition training."
+        />
+        <meta property="og:url" content="https://jaykacademy.com" />
+        <meta property="og:type" content="website" />
+        <meta name="robots" content="index,follow" />
+        <meta
+          name="keywords"
+          content="research, thesis, paper, dissertation, writing, publication, editing, proof reading, academic research, academic research training, academic research consultation, hire a writer, project writing, thesis research analysis, dissertation research analysis, professional research, research analysis, skills acquisition, research requirements, research paper, research topic, graduate topic, graduate paper, graduate dissertation, graduate thesis, phd topics, phd thesis, phd coursework, phd dissertation, phd research, phd paper, phd course work, seminar project, seminar paper, project paper, project coursework"
+        />
+      </Head>
       <Navbar activeSection={activeSection} />
       <Hero id="hero" innerRef={sectionRefs[0]} />
       <About id="about" innerRef={sectionRefs[1]} />
